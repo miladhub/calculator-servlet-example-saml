7615fa30d66fdb511b969522dfada9b121b78b4b
milad.bourhani <milad.bourhani@accenture.com>
2020-04-23 10:56:45 +0200

    fixed

--- src/main/java/AuthFilter.java
+++ src/main/java/AuthFilter.java
@@ -15,20 +15,23 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
 
-        String remote_user = req.getHeader("remote_user");
-        System.out.println("remote_user: " + remote_user);
-        System.out.println("shib-assertion-01: " + req.getHeader("shib-assertion-01"));
+        String uid = req.getHeader("uid");
+        System.out.println("uid: " + uid);
+        String assertionUrl = req.getHeader("shib-assertion-01");
+        System.out.println("shib-assertion-01: " + assertionUrl);
 
         String token = req.getParameter("my_csrf_token");
         if (token == null) {
-            if (remote_user != null) { // only if using SSO
+            if (uid != null) { // only if using SSO
     //        resp.addHeader("Token", "42");
-                req.setAttribute("my_csrf_token", remote_user);
-                resp.setHeader("Set-Cookie", "CalcSession=" + remote_user + "; HttpOnly; SameSite=strict");
+                req.setAttribute("my_csrf_token", uid);
+                resp.setHeader("Set-Cookie", "CalcSession=" + uid + "; HttpOnly; SameSite=strict");
                 chain.doFilter(request, response);
             } else {
                 resp.sendError(401);
             }
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
